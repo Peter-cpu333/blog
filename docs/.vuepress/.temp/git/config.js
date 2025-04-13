a60a6a9f279c9eae8ad013b567ab135c24964a87
Peter <Zhuyq0719@foxmail.com>
2025-04-13 18:22:32 +0800

    初始化

--- docs/.vuepress/.temp/git/config.js
+++
@@ -1,9 +0,0 @@
-import { GitContributors } from "/Users/zhuyq0719/Documents/vuepress-starter/node_modules/@vuepress/plugin-git/lib/client/components/GitContributors.js";
-import { GitChangelog } from "/Users/zhuyq0719/Documents/vuepress-starter/node_modules/@vuepress/plugin-git/lib/client/components/GitChangelog.js";
-
-export default {
-  enhance: ({ app }) => {
-    app.component("GitContributors", GitContributors);
-    app.component("GitChangelog", GitChangelog);
-  },
-};
